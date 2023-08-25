8080d2add5cc0ee9bb71522656857e5b1450fcf0
Alena <alena.yarmak@protonmail.com>
2023-08-25 19:01:29 +0200

    fix glith with images onHover

---
+++ js/imgPreloading.js
@@ -0,0 +1,11 @@
+let hoverEmail = new Image();
+let hoverPhone = new Image();
+let hoverTelegram = new Image();
+let hoverViber = new Image();
+let hoverWhatsapp = new Image();
+
+hoverEmail.src = '../img/socials/hover-email.png';
+hoverPhone.src = '../img/socials/hover-phone.png';
+hoverTelegram.src = '../img/socials/hover-telegram.png';
+hoverViber.src = '../img/socials/hover-viber.png';
+hoverWhatsapp.src = '../img/socials/hover-whatsapp.png';
