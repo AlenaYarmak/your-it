df91d672cea6dd759074103f953c36a6137372ad
Alena <alena.yarmak@protonmail.com>
2023-09-05 13:18:34 +0200

    add hiding labels in inputs on click

--- js/form.js
+++ js/form.js
@@ -1,9 +1,18 @@
-let input = document.querySelectorAll('.form-input');
+let inputs = document.querySelectorAll('.form-input');
 
-input.forEach(function(item) {
-    item.addEventListener('click', function() {
-            console.log('good');
-            console.log(item);
+inputs.forEach(function(input) {
+    input.addEventListener('focus', function() {
+        let label = this.nextElementSibling;
+        if (label && label.classList.contains('form-placeholder')) {
+            label.style.display = 'none';
+        }
+    })
+
+    input.addEventListener('blur', function() {
+        let label = this.nextElementSibling;
+        if (label && label.classList.contains('form-placeholder') && this.value === '') {
+            label.style.display = 'block';
+        }
     })
 })
 
