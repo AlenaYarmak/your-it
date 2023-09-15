35990946934ab464f90c0bd6c70cc6b50ed5f262
Alena <alena.yarmak@protonmail.com>
2023-09-15 11:56:04 +0200

    add .gitignore / add server folder / adjust server for accept user data(initial settings)

--- js/form.js
+++ js/form.js
@@ -1,4 +1,13 @@
+/* const { response } = require("express"); */
+
+let form = document.querySelector('.form');
+
 let inputs = document.querySelectorAll('.form-input');
+
+let userName = document.getElementById('name');
+let userEmail = document.getElementById('email');
+let userPhone = document.getElementById('phone');
+let userMessage = document.getElementById('message');
 
 inputs.forEach(function(input) {
     input.addEventListener('focus', function() {
@@ -14,5 +23,37 @@ inputs.forEach(function(input) {
             label.style.display = 'block';
         }
     })
+})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const userData = {
+        name: userName.value,
+        email: userEmail.value,
+        phone: userPhone.value,
+        message: userMessage.value
+    }
+
+    const json = JSON.stringify(userData);
+
+    console.log(json);
+
+    fetch('http://localhost:3000/submit', {
+        method: 'POST',
+        body: json,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the server's response (e.g., show a confirmation message)
+        console.log(response.json);
+    })
+    .catch(error => {
+        // Handle errors (e.g., show an error message)
+    });
+
 })
 
